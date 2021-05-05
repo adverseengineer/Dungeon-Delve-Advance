f67d7667da96e073ff7e3d119ba795573c40a04a
Nick <adverseengineer@gmail.com>
2021-05-05 03:52:42 -0400

    asset converter script works
    it's stupid slow though. something about image magick appending to the same file too quickly

--- convert-assets.sh
+++ convert-assets.sh
@@ -1,6 +1,23 @@
 #!/usr/bin/env sh
 
 cd graphics
-for FILE in $(ls *.ase); do
-	libresprite -b $FILE --sheet "$(basename $FILE .ase).png" --data /dev/null --sheet-type columns --verbose;
+#for each .ase file
+for FILE in *.ase; do
+	#create a new directory to work in
+	mkdir temp
+	#generate the new filename
+	NEWNAME="temp/$(basename $FILE .ase).png"
+	#export the ase as a png
+	libresprite -b $FILE --sheet $NEWNAME --data /dev/null;
+	#split the exported png into 16x16 tiles
+	magick convert $NEWNAME -crop 16x16 temp/tile.png
+	#store the name of the resulting gba-compatible sheet
+	FINALNAME="$(basename $FILE .ase).png"
+	touch $FINALNAME
+	#for each tile
+	for TILE in $(ls temp/tile*.png | sort --version-sort); do
+		#append it to the generated vertical strip sheet
+		magick convert -append $FINALNAME $TILE $FINALNAME
+	done
+	rm -rf temp
 done
