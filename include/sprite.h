57376eaba47f033bd1fba0de3be28c26fc6e947c
Nick <adverseengineer@gmail.com>
2021-06-12 22:29:26 -0400

    replaced linked list oam scheme with smart array
    this commit won't build though because i only committed the sprite changes

--- include/sprite.h
+++ include/sprite.h
@@ -2,13 +2,13 @@
 //sprite.h
 
 //provides a layer of abstraction away from the hardware for dealing with sprites
-//uses the linked-list scheme described at https://www.gamasutra.com/view/feature/131491/gameboy_advance_resource_management.php
 
 #ifndef SPRITE_H
 #define SPRITE_H
 
-#include <stdlib.h>
 #include <tonc.h>
+
+#include "config.h"
 
 //=============================================================================
 //DEFINES
@@ -22,92 +22,34 @@
 
 typedef struct Sprite Sprite;
 struct Sprite {
-	OBJ_ATTR obj;
-	BOOL onScreen;
-	Sprite* next;
+	OBJ_ATTR obj; //a group of attributes that make up a sprite in hardware
+	POINT16 pos; //the "true" position. necessary to avoid coordinate wrapping
+	BOOL isAvailable; //whether or not the sprite can be safely overwritten
+	BOOL needsRedraw; //whether or not to rewrite this sprite's obj to oam
 } ALIGN4;
 
 //=============================================================================
-//EXTERNS
+//FUNCTION DECLARATIONS
 //=============================================================================
 
-extern BOOL spr_needRedraw;
+void spr_init(void);
 
-extern void spr_init(void);
-extern void spr_link(Sprite* self);
-extern void spr_unlink(Sprite* self);
-extern void spr_render(void);
+void spr_render(void);
 
-//=============================================================================
-//INLINES
-//=============================================================================
+Sprite* spr_create(s32 x, s32 y, ObjTile tid, ObjPrio prio);
+void spr_destroy(Sprite* self);
 
-//allocates and initializes a sprite
-//NOTE: for now, assumes a square 16x16 sprite, because that's all i'm using
-static inline Sprite* spr_create(s32 x, s32 y, u32 tid, u32 prio) {
-	//allocate a sprite
-	Sprite* self = (Sprite*) malloc(sizeof(Sprite));
-	obj_set_attr(&self->obj, ATTR0_SQUARE, ATTR1_SIZE_16, ATTR2_BUILD(tid, 0, prio));
-	obj_set_pos(&self->obj, x, y);
-	return self;
-}
+s32 spr_getX(const Sprite* self);
+void spr_setX(Sprite* self, s32 x);
+s32 spr_getY(const Sprite* self);
+void spr_setY(Sprite* self, s32 x);
 
-//deallocates a sprite
-static inline void spr_destroy(Sprite* self) {
-	free(self);
-}
+void spr_setPos(Sprite* self, s32 x, s32 y);
+void spr_move(Sprite* self, s32 dx, s32 dy);
 
-//gets a sprite's x coord
-static inline s32 spr_getX(const Sprite* self) {
-	return BFN_GET(self->obj.attr1, ATTR1_X);
-}
+void spr_flipHorz(Sprite* self);
+void spr_flipVert(Sprite* self);
 
-//sets a sprite's x coord
-inline void spr_setX(Sprite* self, s32 x) {
-	BFN_SET(self->obj.attr1, x, ATTR1_X);
-	spr_needRedraw = TRUE;
-}
-
-//gets a sprite's y coord
-inline s32 spr_getY(const Sprite* self) {
-	return BFN_GET(self->obj.attr0, ATTR0_Y);
-}
-
-//sets a sprite's y coord
-inline void spr_setY(Sprite* self, s32 x){
-	BFN_SET(self->obj.attr0, x, ATTR0_Y);
-	spr_needRedraw = TRUE;
-}
-
-//moves the given sprite to the given coords
-static inline void spr_setPos(Sprite* self, s32 x, s32 y) {
-	obj_set_pos(&self->obj, x, y);
-	spr_needRedraw = TRUE;
-}
-
-//moves the given sprite relative to it's current position
-static inline void spr_move(Sprite* self, s32 dx, s32 dy) {
-	spr_setX(self, dx + spr_getX(self));
-	spr_setY(self, dy + spr_getY(self));
-	spr_needRedraw = TRUE;
-}
-
-//flips a sprite horizontally
-inline void spr_flipHorz(Sprite* self) {
-	self->obj.attr1 ^= ATTR1_HFLIP;
-	spr_needRedraw = TRUE;
-}
-
-//flips a sprite vertically
-inline void spr_flipVert(Sprite* self) {
-	self->obj.attr1 ^= ATTR1_VFLIP;
-	spr_needRedraw = TRUE;
-}
-
-//returns true if a sprite is within the screen bounds, give or take 16 pixels
-static inline BOOL spr_onScreen(Sprite* self) {
-	return in_range(spr_getX(self), -16, SCREEN_WIDTH + 16)
-		&& in_range(spr_getY(self), -16, SCREEN_HEIGHT + 16);
-}
+BOOL spr_isOnScreen(const Sprite* self);
 
 #endif //SPRITE_H
--- source/sprite.c
+++ source/sprite.c
@@ -7,82 +7,119 @@
 //GLOBALS
 //=============================================================================
 
-BOOL spr_needRedraw;
-
-static Sprite* head;
-static u8 sprite_counter;
+static Sprite sprites[MAX_SPRITES];
 
 //=============================================================================
 //FUNCTIONS
 //=============================================================================
 
-//set the proper video flags and clears the sprite list AND the real oam to safe values
+//clears the sprite list AND the real oam to safe values
 void spr_init(void) {
-	Sprite* current = head;
-	while(current != NULL) {
-   		obj_hide(&current->obj);
-		current = current->next;
+	for(size_t i = 0; i < MAX_SPRITES; i++) {
+		obj_hide(&sprites[i].obj);
+		sprites[i].isAvailable = TRUE;
 	}
+
 	oam_init(oam_mem, MAX_SPRITES);
 }
 
-//links a sprite into the shadow oam list
-void spr_link(Sprite* spr) {
-	//only create if there is room
-	if(sprite_counter < MAX_SPRITES) {
-		sprite_counter++;
-		//link spr into the beginning of the list
-		spr->next = head;
-		head = spr;
-
-		spr_needRedraw = TRUE;
-	}
-}
-
-
-//delete a link with given key
-void spr_unlink(Sprite* spr) {
-	//only destroy if there is at least one sprite
-	if((sprite_counter > 0) && (head != NULL)) {
-		sprite_counter--;
-
-		Sprite* current = head;
-		Sprite* previous = NULL;
-		//until we find the target sprite
-		while(current != spr) {
-			//if we hit the end searching for it, abort
-			if(current->next == NULL)
-				return;
-
-			//otherwise, update the trackers for the next loop iteration
-			previous = current;
-			current = current->next;
+//claims the next available sprite slot, sets it up, and returns a pointer to it
+Sprite* spr_create(s32 x, s32 y, ObjTile tid, ObjPrio prio) {
+	
+	for(size_t i = 0; i < MAX_SPRITES; i++) {
+		if(sprites[i].isAvailable) {
+			//claim the slot
+			Sprite* self = &sprites[i];
+			self->isAvailable = FALSE;
+			self->needsRedraw = TRUE;
+			//set up the sprite
+			obj_set_attr(&self->obj, ATTR0_SQUARE, ATTR1_SIZE_16, ATTR2_BUILD(tid, 0, prio));
+			spr_setPos(self, x, y);
+			return self;
 		}
-		//if the target is the head of the list, the head is replaced by it's next.
-		if(current == head)
-			head = head->next;
-		//otherwise, stitch spr's neighbors together, excluding spr
-		else
-			previous->next = current->next;
-
-		spr_needRedraw = TRUE;
 	}
+	//if no slot was available,
+	mgba_printf(LOG_ERR, "failed to create sprite: no sprite slots available");
+	return NULL;
 }
 
+//relinquishes a sprite slot
+void spr_destroy(Sprite* self) {
+	self->isAvailable = TRUE;
+	obj_hide(&self->obj);
+}
 
 //renders all sprites in the list
 void spr_render(void) {
-	if(spr_needRedraw) {
-		Sprite* current = head;
-		u32 i;
-		for(i = 0; current != NULL; i++) {
-			oam_copy(&oam_mem[i], &current->obj, 1);
-			current = current->next;
+	for(size_t i = 0; i < MAX_SPRITES; i++) {
+		if(!sprites[i].isAvailable && sprites[i].needsRedraw) {
+			oam_copy(&oam_mem[i], &sprites[i].obj, 1);
+			sprites[i].needsRedraw = FALSE;
 		}
-		
-		obj_hide_multi(&oam_mem[i], MAX_SPRITES - i);
-
-		spr_needRedraw = FALSE;
 	}
+}
+
+//gets a sprite's x coord
+s32 spr_getX(const Sprite* self) {
+	return BFN_GET(self->obj.attr1, ATTR1_X);
+}
+
+//sets a sprite's x coord
+void spr_setX(Sprite* self, s32 x) {
+	BFN_SET(self->obj.attr1, x, ATTR1_X);
+	self->pos.x = x;
+	self->needsRedraw = TRUE;
+}
+
+//gets a sprite's y coord
+s32 spr_getY(const Sprite* self) {
+	return BFN_GET(self->obj.attr0, ATTR0_Y);
+}
+
+//sets a sprite's y coord
+void spr_setY(Sprite* self, s32 y){
+	BFN_SET(self->obj.attr0, y, ATTR0_Y);
+	self->pos.y = y;
+	self->needsRedraw = TRUE;
+}
+
+//moves the given sprite to the given coords
+void spr_setPos(Sprite* self, s32 x, s32 y) {
+	spr_setX(self, x);
+	spr_setY(self, y);
+}
+
+//moves the given sprite relative to it's current position
+void spr_move(Sprite* self, s32 dx, s32 dy) {
+	spr_setX(self, dx + spr_getX(self));
+	spr_setY(self, dy + spr_getY(self));
+}
+
+//flips a sprite horizontally
+void spr_flipHorz(Sprite* self) {
+	self->obj.attr1 ^= ATTR1_HFLIP;
+	self->needsRedraw = TRUE;
+}
+
+//flips a sprite vertically
+void spr_flipVert(Sprite* self) {
+	self->obj.attr1 ^= ATTR1_VFLIP;
+	self->needsRedraw = TRUE;
+}
+
+void spr_setVisibility(Sprite* self, BOOL visible) {
+	if(visible)
+		obj_unhide(&self->obj, 0); //TODO: what is the second param supposed to be???
+	else
+		obj_hide(&self->obj);
+
+	self->needsRedraw = TRUE;
+}
+
+//TODO: replace 16 with the width or height of the sprite
+//returns true if any part of the sprite is within the screen rect
+BOOL spr_isOnScreen(const Sprite* self) {
+	return in_range(self->pos.x, -16, SCREEN_WIDTH)
+		&& in_range(self->pos.y, -16, SCREEN_HEIGHT);
 }
 
